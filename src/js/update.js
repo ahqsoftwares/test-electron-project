4d9a2b2f62769020f6677713f3a7bbd4f6cc55a2
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-04-12 23:14:09 +0530

    maybe a fix?

--- src/js/update.js
+++ src/js/update.js
@@ -1,21 +1,27 @@
-document.getElementById("updating").innerText = "Confirming Update Check";
-const { autoUpdater } = require("electron-updater");
-const { Notification } = require("electron");
+const uaup = require('uaup-js');
 
 
-autoUpdater.checkForUpdates()
-autoUpdater.on('update-available', () => {
-    document.getElementById("updating").innerText = "Starting Download...";
-    autoUpdater.downloadUpdate()
-    autoUpdater.on('update-downloaded', async() => {
-        document.getElementById("updating").innerText = "Starting Install";
-        await new Notification({title: "Starting Install!", body: "Installing Update!"}).show()
-        autoUpdater.quitAndInstall({
-            isSilent: true,
-            isForceRunAfter: true
-        });
-    });
-});
-autoUpdater.on("error", async() => {
-    document.getElementById("updating").innerText = "Error! Update from https://github.com/ahqsoftwares/test-electron-project";
-});
+//This is Optional
+const defaultStages = {
+    Checking: "Checking...", // When Checking For Updates.
+    Found: "Update Found!",  // If an Update is Found.
+    NotFound: "No Update Found.", // If an Update is Not Found.
+    Downloading: "Downloading...", // When Downloading Update.
+    Unzipping: "Installing...", // When Unzipping the Archive into the Application Directory.
+    Cleaning: "Finalizing...", // When Removing Temp Directories and Files (ex: update archive and tmp directory).
+    Launch: "Launching..." // When Launching the Application.
+};
+
+const updateOptions = {
+    gitRepo: "test-electron-project", // [Required] Your Repo Name
+    gitUsername: "ahqsoftwares",  // [Required] Your GitHub Username.
+
+    appName: "electron-project", //[Required] The Name of the app archive and the app folder.
+    appExecutableName: "electron-project.exe", //[Required] The Executable of the Application to be Run after updating.
+
+    progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
+    label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
+    stageTitles: defaultStages, // {Default is defaultStages} [Optional] Sets the Status Title for Each Stage
+};
+
+uaup.Update(updateOptions);
