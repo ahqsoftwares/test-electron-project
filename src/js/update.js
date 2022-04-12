becc2d8c25780b5d4aaa7adff0a5d0eb4514618b
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-04-12 22:11:57 +0530

    updater fix

--- src/js/update.js
+++ src/js/update.js
@@ -2,22 +2,20 @@ document.getElementById("updating").innerText = "Confirming Update Check";
 const { autoUpdater } = require("electron-updater");
 const { Notification } = require("electron");
 
-function updateCheck() {
-    autoUpdater.checkForUpdates()
-}
-async function start() {
+
+autoUpdater.checkForUpdates()
+autoUpdater.on('update-available', () => {
+    document.getElementById("updating").innerText = "Starting Download...";
     autoUpdater.downloadUpdate()
     autoUpdater.on('update-downloaded', async() => {
         document.getElementById("updating").innerText = "Starting Install";
         await new Notification({title: "Starting Install!", body: "Installing Update!"}).show()
-        autoUpdater.quitAndInstall()
+        autoUpdater.quitAndInstall({
+            isSilent: true,
+            isForceRunAfter: true
+        });
     });
-}
-async function load() {
-    updateCheck()
-    autoUpdater.on('update-available', () => {
-        document.getElementById("updating").innerText = "Starting Download...";
-        start(true);
-    });
-}
-load()
+});
+autoUpdater.on("error", async() => {
+    document.getElementById("updating").innerText = "Error! Update from https://github.com/ahqsoftwares/test-electron-project";
+});
